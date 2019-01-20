8a8fbb91b6209befcf03c25155bd19ad6b4f93e1
Vladislav <klychik-nsk54rus@mail.ru>
2019-01-20 22:51:47 +0700

    что-то двигается, штраф не работает
    1

---
+++ Compute_Potential_Energy.m
@@ -0,0 +1,30 @@
+function [P] =Compute_Potential_Energy(x,xper,xc,yc,l,N) %initialization(L,H,N,flag)
+%for now size fixed (2,2)
+%flag 1 -upper plate -1 -lower plate
+%N_y = fix(sqrt(N*L/H));
+%N_x = fix( N / N_y);
+Energy=0;
+Penetrationx=0;
+Penetrationy=0;
+Penetration=0;
+for  j=1:N
+     for  i=1:j
+         if(i==j)
+             continue;
+         end
+         
+         Penetrationx=abs(x(1,i)-xc)-l/2;
+            if (Penetrationx<0)
+                Penetrationx=0;
+            end
+         Penetrationy=abs(x(2,i)-yc)-l/2;
+            if (Penetrationy<0)
+                Penetrationy=0;
+            end
+        Penetration=Penetrationx+Penetrationy;
+        Energy=Energy+1/sqrt((xper(1,i)-x(1,j))*(xper(1,i)-x(1,j))+(xper(2,i)-x(2,j))*(xper(2,i)-x(2,j)));
+        Energy=Energy+Penetration;
+     end
+end
+
+P=Energy;
--- base.m
+++ base.m
@@ -1,10 +1,55 @@
 clear;
 N=100;
-x_t_r=1;
-y_t_r=1;
-x_0=0;
-y_0=0;
-r=0.5;
+nu=1;
+k=1;
+l=1;
 
-x=(2,N);
-P=(1,N);
+dt=0.000001;
+dh=0.000001;
+
+x_top_r=1;
+y_top_r=1;
+
+xc=x_top_r/2;
+yc=y_top_r/2;
+
+x=zeros(2,N);
+xper1=zeros(2,N);
+xper2=zeros(2,N);
+Energy=0;
+EnergyX=0;
+EnergyY=0;
+
+Penetrationx=0;
+Penetrationy=0;
+
+for i=1:N 
+    x(1,i)=(1)*rand;
+    x(2,i)=(1)*rand;
+end 
+
+for time=1:300
+    for i=1:N 
+        Energy=Compute_Potential_Energy(x,x,N);
+        
+        xper=x;
+        xper1(1,i)=xper(1,i)+dh;
+        
+        xper2=x;
+        xper2(2,i)=xper(2,i)+dh;
+        
+        EnergyX=Compute_Potential_Energy(x,xper,N);
+        Penetrationx=Compute_Penetration(x,xc,l,i);
+        x(i)=x(i)-dt/nu*(EnergyX-Energy)/dh;
+        
+       EnergyY=Compute_Potential_Energy(x,xper2,N);
+       Penetrationy=Compute_Penetration(x,yc,l,i);
+       x(i)=x(i)-dt/nu*(EnergyY-Energy)/dh;
+        
+       
+    end
+     plot(x(1,1:N),x(2,1:N),'.')
+        pause(0.0001)
+end
+
+ 
